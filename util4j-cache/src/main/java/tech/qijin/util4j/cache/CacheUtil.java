03a8143ff2555abc1f3dda06369170081e59ee48
Yang Shangqiang <michealyang@aviagames.com>
2019-01-04 11:45:16 +0800

    [util4j-cache] 待修改的分布式缓存

--- util4j-cache/src/main/java/tech/qijin/util4j/cache/CacheUtil.java
+++ util4j-cache/src/main/java/tech/qijin/util4j/cache/CacheUtil.java
@@ -6,4 +6,20 @@ package tech.qijin.util4j.cache;
  * 开始做眼保健操：←_← ↑_↑ →_→ ↓_↓
  **/
 public class CacheUtil {
+
+    public static boolean setnx(String key, long timestamp) {
+        return true;
+    }
+
+    public static Object get(String key) {
+        return null;
+    }
+
+    public static boolean compareAndSet(String key, Long old, Long timestamp) {
+        return true;
+    }
+
+    public static boolean delete(String key) {
+        return true;
+    }
 }
