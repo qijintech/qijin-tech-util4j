f9e63f42b2cbf531e01c0fad5d708a416ec335a9
Yang Shangqiang <michealyang@aviagames.com>
2019-01-04 11:43:49 +0800

    [util4j-utils] 增加数字类型参数校验方法

---
+++ util4j-utils/src/main/java/tech/qijin/util4j/utils/NumberUtil.java
@@ -0,0 +1,66 @@
+package tech.qijin.util4j.utils;
+
+/**
+ * @author michealyang
+ * @version 1.0
+ * @created 18/6/15
+ * 开始眼保健操： →_→  ↑_↑  ←_←  ↓_↓
+ */
+public class NumberUtil {
+    /**
+     * 判断是否为null或==0
+     *
+     * @param value
+     * @return
+     */
+    public static boolean nullOrZero(Number value) {
+        if (value == null) {
+            return true;
+        }
+        if (value instanceof Integer) {
+            return ((Comparable) value).compareTo(0) == 0;
+        }
+        if (value instanceof Long) {
+            return ((Comparable) value).compareTo(0L) == 0;
+        }
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * 是否为null或者<=0
+     *
+     * @param value
+     * @return
+     */
+    public static boolean nullOrLeZero(Number value) {
+        if (value == null) {
+            return true;
+        }
+        if (value instanceof Integer) {
+            return ((Comparable) value).compareTo(0) < 0;
+        }
+        if (value instanceof Long) {
+            return ((Comparable) value).compareTo(0L) < 0;
+        }
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * 判断是否为null或<=0
+     *
+     * @param value
+     * @return
+     */
+    public static boolean nullOrLtZero(Number value) {
+        if (value == null) {
+            return true;
+        }
+        if (value instanceof Integer) {
+            return ((Comparable) value).compareTo(0) <= 0;
+        }
+        if (value instanceof Long) {
+            return ((Comparable) value).compareTo(0L) <= 0;
+        }
+        throw new UnsupportedOperationException();
+    }
+}
