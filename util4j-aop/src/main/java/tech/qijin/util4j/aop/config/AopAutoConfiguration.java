262537397882103900f2c4782737605298b8f14c
michealyang <michealyang@aviagames.com>
2019-03-11 19:00:41 +0800

    [util4j-aop] 支持auto config

---
+++ util4j-aop/src/main/java/tech/qijin/util4j/aop/config/AopAutoConfiguration.java
@@ -0,0 +1,14 @@
+package tech.qijin.util4j.aop.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author michealyang
+ * @date 2019/3/8
+ * 开始做眼保健操：←_← ↑_↑ →_→ ↓_↓
+ **/
+@Configuration
+@ComponentScan("tech.qijin.util4j.aop")
+public class AopAutoConfiguration {
+}
