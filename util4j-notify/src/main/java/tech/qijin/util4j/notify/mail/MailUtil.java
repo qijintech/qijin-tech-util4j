5e77a08295fecb0bb9a4ba4d2a27a40deac66edc
michealyang <michealyang@aliyun.com>
2019-03-20 14:04:36 +0800

    [util4j-notify] 添加了一个测试类

--- util4j-notify/src/main/java/tech/qijin/util4j/notify/mail/MailUtil.java
+++ util4j-notify/src/main/java/tech/qijin/util4j/notify/mail/MailUtil.java
@@ -1,11 +1,12 @@
 package tech.qijin.util4j.notify.mail;
 
 /**
- * 需要确保简单的异步
+ * 使用jodd包
  *
  * @author michealyang
  * @date 2018/11/27
  * 开始做眼保健操：←_← ↑_↑ →_→ ↓_↓
+ * @refer https://jodd.org/
  **/
 public class MailUtil {
 }
---
+++ util4j-notify/src/test/java/tech/qijin/util4j/notify/test/MailTest.java
@@ -0,0 +1,32 @@
+package tech.qijin.util4j.notify.test;
+
+import jodd.mail.Email;
+import jodd.mail.MailServer;
+import jodd.mail.SendMailSession;
+import jodd.mail.SmtpServer;
+import org.junit.Test;
+
+/**
+ * @author michealyang
+ * @date 2019/3/15
+ * 开始做眼保健操：←_← ↑_↑ →_→ ↓_↓
+ **/
+public class MailTest {
+    @Test
+    public void test() {
+        Email email = Email.create()
+                .from("john@jodd.org")
+                .to("michealyang@aliyun.com")
+                .subject("Hello!")
+                .textMessage("A plain text message...");
+        SmtpServer smtpServer = MailServer.create()
+                .host("smtp.sendgrid.net")
+                .port(587)
+                .auth("mail_test2", "aviagames2019")
+                .buildSmtpMailServer();
+        SendMailSession session = smtpServer.createSession();
+        session.open();
+        session.sendMail(email);
+        session.close();
+    }
+}
