363cfa5824372727c987ebf22125f56d3590ab9a
michealyang <yangshangqiang@inke.cn>
2021-09-02 23:32:34 +0800

    update

--- util4j-lang/src/main/java/tech/qijin/util4j/lang/template/ICacheKey.java
+++ util4j-lang/src/main/java/tech/qijin/util4j/lang/template/ICacheKey.java
@@ -11,7 +11,7 @@ import org.apache.commons.lang3.StringUtils;
  **/
 public interface ICacheKey {
 
-    String KEY_SEPARATOR = ".";
+    String KEY_SEPARATOR = ":";
 
     /**
      * 返回对应module的名字
